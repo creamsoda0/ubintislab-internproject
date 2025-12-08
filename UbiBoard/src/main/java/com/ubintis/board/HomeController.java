c9254cd2a246b626fc1a79878fac8b926cfd382a
UBINTIS-EWHA1\ubintis <dbs0877@gmail.com>
2025-12-09 08:43:21 +0900

    index.jsp 기초 컨트롤러 modelandview로 전환하여 테스트 완료

--- UbiBoard/src/main/java/com/ubintis/board/HomeController.java
+++ UbiBoard/src/main/java/com/ubintis/board/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -22,18 +23,16 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping(value = "/index"/* , method = RequestMethod.GET */)
+	public ModelAndView home(Locale locale, Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		ModelAndView mav = new ModelAndView();
 		
-		String formattedDate = dateFormat.format(date);
+		//¿¹½Ã
+		/* mav.addObject("userName", "creamsoda"); */
+		mav.setViewName("index");
 		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return mav;
 	}
 	
 }
